72df247b63e0e6705c91ec76032d697bd258ca72
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-05-07 20:53:06 +0000

    chore: Rust 1.78.0 linting

--- src/app_env.rs
+++ src/app_env.rs
@@ -150,18 +150,15 @@ impl AppEnv {
         let current_exe_dir =
             env::current_exe().map_or(None, |p| p.ancestors().nth(1).map(|i| i.join(".env")));
 
-        let env_path = dotenvy::dotenv().map_or_else(
-            |_| {
-                current_exe_dir.map_or_else(
-                    || {
-                        println!("\n\x1b[31munable to load env file\x1b[0m\n");
-                        std::process::exit(1);
-                    },
-                    |current_exe_dir| current_exe_dir,
-                )
-            },
-            |env| env,
-        );
+        let env_path = dotenvy::dotenv().unwrap_or_else(|_| {
+            current_exe_dir.map_or_else(
+                || {
+                    println!("\n\x1b[31munable to load env file\x1b[0m\n");
+                    std::process::exit(1);
+                },
+                |current_exe_dir| current_exe_dir,
+            )
+        });
 
         dotenvy::from_path(env_path).ok();
         match Self::generate() {
